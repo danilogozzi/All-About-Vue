a894bbc321f5523d854c8d822516fd42cbab0a5f
Danilo <62484573+danilogozzi@users.noreply.github.com>
2022-04-22 03:54:52 -0300

    Add files via upload

---
+++ aulaextra-js-ts/destructuring/destructuring.ts
@@ -0,0 +1,17 @@
+//----------------------------Ojects
+const pessoa = {
+    nome: 'Danilo',
+    idade: 27,
+    endereco: {
+        logradouro: 'Nair',
+        numero: 27
+    }
+}
+
+const {nome: n, idade: i} = pessoa
+console.log(n)
+console.log(i)
+
+const {endereco: {logradouro, numero}} = pessoa
+console.log(logradouro, numero)
+
---
+++ aulaextra-js-ts/identificador/identificador.ts
@@ -0,0 +1,15 @@
+var a: number = 3
+let b: number = 4
+
+var a = 30
+b = 40
+
+console.log(a, b)
+
+a = 300
+b = 400
+
+console.log(a, b)
+
+const c: number = 5
+console.log(c)
